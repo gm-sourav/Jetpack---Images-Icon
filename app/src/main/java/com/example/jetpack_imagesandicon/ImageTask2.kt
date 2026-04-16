f68b622b814fc65a8deacda30bd7bcb336a95393
gm-sourav <souravmahbub13@gmail.com>
2026-04-16 23:22:52 +0600

    First

---
+++ app/src/main/java/com/example/jetpack_imagesandicon/ImageTask2.kt
@@ -0,0 +1,46 @@
+package com.example.jetpack_imagesandicon
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ImageTask2 () {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.green_grass),
+            contentDescription = "Green Grass",
+            modifier = Modifier
+                .size(300.dp)
+                .clip(CircleShape)
+                .border(3.dp, White, CircleShape),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop
+
+
+        )
+    }
+}
