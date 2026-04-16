7e225d72c441455a28d5cc64ff766668be2b278c
gm-sourav <souravmahbub13@gmail.com>
2026-04-16 22:37:39 +0600

    initial commit

---
+++ app/src/main/java/com/example/jetpack_imagesandicon/IconsBasic.kt
@@ -0,0 +1,33 @@
+package com.example.jetpack_imagesandicon
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+ fun IconExmp () {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Default.Person,
+            contentDescription = "Person Icon",
+            modifier = Modifier.size(100.dp),
+            tint = Color.Red
+        )
+    }
+}
