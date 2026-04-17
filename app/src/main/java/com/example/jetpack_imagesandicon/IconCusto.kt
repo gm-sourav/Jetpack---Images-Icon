316c247721dd8a1deb805ca164604088c1e41e71
gm-sourav <souravmahbub13@gmail.com>
2026-04-17 12:13:15 +0600

    2nd

---
+++ app/src/main/java/com/example/jetpack_imagesandicon/IconCusto.kt
@@ -0,0 +1,32 @@
+package com.example.jetpack_imagesandicon
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun IconCus () {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Person, // Outlined, Rounded
+            contentDescription = "Profile Icon",
+            modifier = Modifier
+                .size(200.dp)
+        )
+    }
+}
